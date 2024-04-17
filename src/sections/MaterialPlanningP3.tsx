29ae0a0058d8f1fe87434692745c7ee71bd58b1e
Lars Abbenseth <lab@objektkultur.de>
2024-04-17 15:30:31 +0200

    Add sections

---
+++ src/sections/MaterialPlanningP3.tsx
@@ -0,0 +1,54 @@
+import { Button, Container } from "@mui/material";
+import { StyledCard } from "../components/styledComponets/styledCard";
+import { useContext } from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { SelectInputXML } from "../pages/StartPage";
+
+export default function MaterialPlanningP3() {
+  const { handleNextStep, handleBack } = useContext(SelectInputXML);
+
+  return (
+    <Container maxWidth={"xl"} sx={{ p: 3, position: "relative" }}>
+      <Button
+        variant="contained"
+        onClick={handleBack}
+        sx={{
+          position: "absolute",
+          left: 0,
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        <ArrowBackIosIcon />
+      </Button>
+      <StyledCard
+        sx={{
+          width: { xs: "100%", md: "90%" },
+          height: { xs: "100%", md: "90%" },
+          minWidth: { xs: "100%", md: "300px" },
+          minHeight: { xs: "100%", md: "700px" },
+          mx: "auto",
+          borderRadius: "16px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      ></StyledCard>
+
+      <Button
+        variant="contained"
+        onClick={handleNextStep}
+        sx={{
+          position: "absolute",
+          right: 0,
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        <ArrowForwardIosIcon />
+      </Button>
+    </Container>
+  );
+}
---
+++ src/sections/Result.tsx
@@ -0,0 +1,40 @@
+import { Button, Container } from "@mui/material";
+import { StyledCard } from "../components/styledComponets/styledCard";
+import { useContext } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { SelectInputXML } from "../pages/StartPage";
+
+export default function Result() {
+  const { handleNextStep, handleBack } = useContext(SelectInputXML);
+
+  return (
+    <Container maxWidth={"xl"} sx={{ p: 3, position: "relative" }}>
+      <Button
+        variant="contained"
+        onClick={handleBack}
+        sx={{
+          position: "absolute",
+          left: 0,
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        <ArrowBackIosIcon />
+      </Button>
+      <StyledCard
+        sx={{
+          width: { xs: "100%", md: "90%" },
+          height: { xs: "100%", md: "90%" },
+          minWidth: { xs: "100%", md: "300px" },
+          minHeight: { xs: "100%", md: "700px" },
+          mx: "auto",
+          borderRadius: "16px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      ></StyledCard>
+    </Container>
+  );
+}
