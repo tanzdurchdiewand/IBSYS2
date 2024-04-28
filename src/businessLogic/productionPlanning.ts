55c674ef28847945b9e44588c95f93185c1f474b
Dinh Thien Phan <dithienphan@gmail.com>
2024-04-28 17:29:06 +0200

    PP_PSEUDO

--- src/businessLogic/productionPlanning.ts
+++ src/businessLogic/productionPlanning.ts
@@ -6,24 +6,45 @@ import {
   ProductionForcast,
   PlanningTimeslot,
   PlanningWorkstation,
+  PlanningWarehouseStock,
 } from "../types/productionPlanningTypes";
-export {};
 
+//__Helping Objects
+
+//_Simulated PlanningWarehouseStock 
+//Function fill PlanningWarehouseStock with current values
+
+//Funktion replicate PlanningWarehouseStock for use in Production Planning
 
 //__Produktion Planning
 
 //Define starting Point
-//-with most used workstations?
-//-most avaliabel material
+//-start with most used workstations
+//-start with most avaliabel material
+//-set a Production Order
 
 //Define Contraint with Dependency Mapping or Material Capacity
-//-constrain Material
-//-Production Order
+//constrain Material
+//set already blocked timeslots from previous Period
 
-//Loop
+//Loop at Orders
+
+//Check - Material availabe
+//Check - next free Timeslot
+
+
+//Change - PlanningWarehouseStock
+//Change - Change Timeslot - Umrüstzeit
+//Change - Change Timeslot - Durchlaufzeit
 
 //Endloop
 
 //Evaluating Solution
+//Count - Umrüstzeiten
 
 //Change Starting Point
+//Splitt Production Order from Item with most Umrüstzeiten
+
+//__Result
+//Production Order
+//Time needed for Machienes
