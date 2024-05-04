69e28fdeebaa5ae4afdd4739b585c71504e7e041
LA <labbenseth@gmx.de>
2024-05-04 12:51:32 +0200

    remove useless function

--- src/components/materialPlanningComponents/MaterialPlanningComponent.tsx
+++ src/components/materialPlanningComponents/MaterialPlanningComponent.tsx
@@ -9,7 +9,7 @@ import {
   TextField,
 } from "@mui/material";
 import { useMaterialPlanning } from "../../hooks/useMaterialPlanning";
-import {} from "../../hooks/useNavigationHandlers";
+import { } from "../../hooks/useNavigationHandlers";
 import {
   MaterialPlanningRow,
   PlanningType,
@@ -24,14 +24,6 @@ export default function MaterialPlanningComponent({
   planningType,
 }: Readonly<Props>) {
   const planning = useMaterialPlanning();
-
-  const handleUpdate = (
-    key: string,
-    field: keyof MaterialPlanningRow,
-    value: number
-  ) => {
-    planning?.updateAndRecalculate(key, field, value, planningType);
-  };
 
   const renderInput = (value: number) => {
     return <TextField variant="outlined" size="small" value={value} disabled />;
@@ -105,10 +97,11 @@ export default function MaterialPlanningComponent({
                         size="small"
                         value={value.safetyStock}
                         onChange={(e) =>
-                          handleUpdate(
+                          planning.updateAndRecalculate(
                             key,
                             "safetyStock",
-                            Number(e.target.value)
+                            Number(e.target.value),
+                            planningType
                           )
                         }
                       />
