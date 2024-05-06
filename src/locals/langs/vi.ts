42c7b58136b97ee92a35fc24c2f5000e10adf4c3
Noah <81570626+tanzdurchdiewand@users.noreply.github.com>
2024-05-06 19:45:41 +0200

    refactor: Update language code for Vietnamese localization

---
+++ src/locals/langs/vi.ts
@@ -0,0 +1,29 @@
+const vi = {
+  navBar: {
+    xmlUpload: "Tải lên XML",
+    productionProgramm: "Chương trình Sản xuất",
+    materialPlanning: "Kế hoạch Vật liệu",
+    orderPlanning: "Kế hoạch Đơn hàng",
+    productionPlanning: "Kế hoạch Sản xuất",
+    capacityPlanningOverview: "Tổng quan Kế hoạch Năng lực",
+    capacityPlanningTotal: "Kế hoạch Năng lực Tổng",
+    result: "Kết quả",
+  },
+  navBarText: {
+    xmlUploadText: "Vui lòng tải lên tệp kết quả XML để tiếp tục.",
+    productionProgrammText:
+      "Vui lòng nhập nguyện vọng sản xuất và dự báo cho ba kỳ tới.",
+    materialPlanningText:
+      "Vui lòng xem xét kế hoạch vật liệu và nhập các thay đổi mong muốn.",
+    orderPlanningText:
+      "Vui lòng xem xét kế hoạch đơn hàng và nhập các thay đổi mong muốn.",
+    productionPlanningText:
+      "Vui lòng xem xét kế hoạch sản xuất và nhập các thay đổi mong muốn.",
+    capacityPlanningOverviewText:
+      "Vui lòng xem xét tổng quan kế hoạch năng lực và nhập các thay đổi mong muốn.",
+    capacityPlanningTotalText: "Vui lòng xem xét kế hoạch năng lực tổng.",
+    resultText: "Kết quả của mô phỏng.",
+  },
+};
+
+export default vi;
--- src/locals/langs/vit.ts
+++
@@ -1,3 +0,0 @@
-const vit = {};
-
-export default vit;
