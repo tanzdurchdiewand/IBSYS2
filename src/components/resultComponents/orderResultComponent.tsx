dc1038846093097bc469d8c4504ef2457f755462
Lars Abbenseth <lab@objektkultur.de>
2024-05-10 10:23:26 +0200

    add orderResultComponent

--- src/components/resultComponents/orderResultComponent.tsx
+++ src/components/resultComponents/orderResultComponent.tsx
@@ -1,1 +1,36 @@
-export {};
+import Paper from "@mui/material/Paper/Paper";
+import Table from "@mui/material/Table/Table";
+import TableBody from "@mui/material/TableBody/TableBody";
+import TableCell from "@mui/material/TableCell/TableCell";
+import TableContainer from "@mui/material/TableContainer/TableContainer";
+import TableHead from "@mui/material/TableHead/TableHead";
+import TableRow from "@mui/material/TableRow/TableRow";
+
+export default function OrderResultComponent() {
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Table
+        sx={{ minWidth: 650 }}
+        aria-label="material planning table"
+        stickyHeader
+        size="small"
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell>Article</TableCell>
+            <TableCell>Quantity</TableCell>
+            <TableCell>Modus</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody></TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
