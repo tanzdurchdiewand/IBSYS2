1c5c5b1f1007ac658bec9c6e3ded8927ab959b2e
Paul Budzinski <pbd@objektkultur.de>
2024-05-10 15:40:47 +0200

    added required time

--- src/types/productionPlanningTypes.ts
+++ src/types/productionPlanningTypes.ts
@@ -38,6 +38,13 @@ export type PlanningWorkstation = {
   maxTime: number;
   availableTime: WeekTime[];
   timeslots: PlanningTimeslot[];
+  productionTimes: ProductionTime[];
+  productionSetupTime: number;
+};
+
+export type ProductionTime = {
+  itemName: string;
+  productionTime: number;
 };
 
 export type PlanningWarehouseStock = {
@@ -60,3 +67,55 @@ export type DirectSellRow = {
   price: number;
   penalty: number;
 };
+
+export const PRODUCTION_SETUP_TIMES: Record<string, number> = {
+  "e49": 6,
+  "e54": 6,
+  "e29": 6,
+  "e50": 5,
+  "e55": 5,
+  "e30": 5,
+  "e51": 5,
+  "e56": 6,
+  "e31": 6,
+  "p1": 6,
+  "p2": 7,
+  "p3": 7,
+  "e16": 2,
+  "e18": 3,
+  "e19": 3,
+  "e20": 3,
+  "e10": 2,
+  "e11": 2,
+  "e12": 2,
+  "e13": 2,
+  "e14": 2,
+  "e15": 2,
+  "e26": 2,
+  "e4": 4,
+  "e5": 4,
+  "e6": 4,
+  "e7": 4,
+  "e8": 4,
+  "e9": 4,
+  "e17": 3,
+};
+
+export const WORKSTATION_SETUP_TIMES: Record<number, number> = {
+  1: 20,
+  2: 30,
+  3: 20,
+  4: 30,
+  5: 0,
+  6: 15,
+  7: 20,
+  8: 20,
+  9: 15,
+  10: 20,
+  11: 20,
+  12: 0,
+  13: 0,
+  14: 0,
+  15: 15,
+};
+
